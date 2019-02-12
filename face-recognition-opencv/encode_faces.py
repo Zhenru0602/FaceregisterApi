42795537fcc805e0c74ee5410fe2ae65609e8066
Anthony Liu <haoyanl2@uci.edu>
2019-02-12 11:53:12 -0800

    Update encode_faces.py

--- face-recognition-opencv/encode_faces.py
+++ face-recognition-opencv/encode_faces.py
@@ -69,7 +69,7 @@ for (i, imagePath) in enumerate(imagePaths):
 			# encodings
 			# knownEncodings.append(encoding)
 			# knownNames.append(name)
-			entry = {'name': name, 'encoding': encoding}
+			entry = {'name': name, 'encoding': encoding.tobytes()}
 			feeds.append(entry)
 		json.dump(feeds, f)
 
