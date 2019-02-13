6a914ea318242168802faa30fe02d56f840ed9fd
ArthLeu <haoyanl2@uci.edu>
2019-02-13 01:31:48 +0000

    cleaned clean_face

--- face-recognition-opencv/clean_faces.py
+++ face-recognition-opencv/clean_faces.py
@@ -1,14 +1,11 @@
 # import the necessary packages
-from imutils import paths
-import face_recognition
-import argparse
 import pickle
-import cv2
 import os
 
 data = pickle.loads(open("encodings.pickle", "rb").read())
-print("cleaning encodings...")
+print("[INFO] cleaning encodings...")
 data = {"encodings": [], "names": []}
 f = open("encodings.pickle", "wb")
 f.write(pickle.dumps(data))
 f.close()
+print("[INFO] cleaning done! Hooray!")
