9903a758830b833b5456a8ad1e8d5d15502b504e
ArthLeu <haoyanl2@uci.edu>
2019-02-14 21:01:54 +0000

    added partial clean face function

--- face-recognition-opencv/clean_faces.py
+++ face-recognition-opencv/clean_faces.py
@@ -1,20 +1,49 @@
 # import the necessary packages
 import pickle
+import argparse
 import shutil
 import os
 import timeit
 
+ap = argparse.ArgumentParser()
+ap.add_argument("-u", "--user", required=False,
+	help="optional selective username")
+args = vars(ap.parse_args())
+username = args["user"]
+
 start = timeit.default_timer()
-data = pickle.loads(open("encodings.pickle", "rb").read())
-print("[INFO] cleaning encodings...")
-data = {"encodings": [], "names": []}
-f = open("encodings.pickle", "wb")
-f.write(pickle.dumps(data))
-f.close()
 
+if not username:
+    print("[INFO] cleaning encodings...")
+    data = {"encodings": [], "names": []}
+    f = open("encodings.pickle", "wb")
+    f.write(pickle.dumps(data))
+    f.close()
 
-shutil.rmtree('dataset/')
-os.mkdir('dataset/')
+    shutil.rmtree('dataset/')
+    os.mkdir('dataset/')
+
+else:
+    if not os.path.isdir("dataset/"+username):
+        print("[ERROR] user not found")
+        exit(0)
+    
+    print("[INFO] cleaning encoding... user", username)
+    data = pickle.loads(open("encodings.pickle", "rb").read())
+    encodings = data["encodings"]
+    names = data["names"]
+    assert len(encodings) == len(names)
+    for i in range(len(names)-1, -1, -1):
+        if names[i] == username:
+            names.pop(i)
+            encodings.pop(i)
+    data = {"encodings":encodings, "names":names}
+    f = open("encodings.pickle", "wb")
+    f.write(pickle.dumps(data))
+    f.close()
+
+    shutil.rmtree("dataset/"+username)
+
 end = timeit.default_timer()
 
 print("[INFO] cleaning done! Hooray!")
